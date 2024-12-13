f559bfe1bde17332931d5d024a955d847efda723
khadija <khadijawakili652@gmail.com>
2024-12-13 17:52:30 +0430

    Re-enabled animations

--- social-media-dashboard-with-theme-switcher-master/src/Components/OverviewCard.jsx
+++ social-media-dashboard-with-theme-switcher-master/src/Components/OverviewCard.jsx
@@ -4,9 +4,9 @@ import {motion} from 'framer-motion'
 const OverviewCard = ({ title, platform, value,Icon, percentage, isNegative }) => {
   return (
   <motion.div
-  // initial={{ opacity: 0, y: 20 }}
-  // animate={{ opacity: 1, y: 0 }}
-  // transition={{ delay: 0.2 }}
+  initial={{ opacity: 0, y: 20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ delay: 0.2 }}
   whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0,0,0,0.5)" }}
    className="p-8 rounded-md shadow-md cursor-pointer bg-opacity-10 bg-gray-500 hover:bg-opacity-25 ">
     <h3 className="text-sm font-medium flex flex-row justify-between">{title}<Icon/></h3>
