09529a506c1c0bc81394559b73bc84b2767d2df7
khadija <khadijawakili652@gmail.com>
2024-12-27 16:57:57 +0430

    Downloaded the huddle-landing-page-with-curved-sections-master challenge

---
+++ huddle-landing-page-with-curved-sections-master/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500  p-4 text-red-200 rounded">
+      Hello, Tailwind with Vite and TypeScript!
+    </div>
+  );
+}
+
+export default App;
