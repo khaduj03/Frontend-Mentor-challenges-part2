d9afade56cafefcb06e8447b957a3edddc2f736d
khadija <khadijawakili652@gmail.com>
2024-12-28 20:32:37 +0430

    created the header

--- huddle-landing-page-with-curved-sections-master/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-blue-500  p-4 text-red-200 rounded">
-      Hello, Tailwind with Vite and TypeScript!
-    </div>
-  );
-}
-
-export default App;
--- huddle-landing-page-with-curved-sections-master/src/component/Header.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Header() {
-  return (
-    <div className='bg-red-500 w-32 h-32'>
-        header
-    </div>
-  )
-}
